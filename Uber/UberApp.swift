3821696c7bf93bc01b31ca8f17d4c64946aa9c29
alx-calcio <alexandre.calciogaudino@gmail.com>
2024-10-13 17:11:52 +0200

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Alexandre Calcio Gaudino on 13/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
